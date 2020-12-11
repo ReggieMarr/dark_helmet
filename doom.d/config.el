82d3c64a90896424eb27836f488f81b40dcec730
Reg Marr <reg.marr@l3harris.com>
2020-12-11 12:43:01 -0500

    Added easy switching between ccls and clangd

--- doom.d/config.el
+++ doom.d/config.el
@@ -209,6 +209,8 @@ currently clocked-in org-mode task."
   ;; (doom-modeline-set-project-modeline) ;; Display current working directory on modeline
   ;; (message "vterm-new-keybindings"))
 
+
+
 (defun show-current-working-dir-in-mode-line ()
   "Shows current working directory in the modeline."
   (interactive)
@@ -372,57 +374,8 @@ currently clocked-in org-mode task."
 )
 
 ;; LSP
-(setq ccls-executable "/usr/bin/ccls")
-(setq ccls-initialization-options '(:index (:comments 2) :completion (:detailedLabel t)))
-(setq ccls-sem-highlight-method 'font-lock)
-;; alternatively, (setq ccls-sem-highlight-method 'overlay)
-
-;; For rainbow semantic highlighting
-;;(ccls-use-default-rainbow-sem-highlight)
-(defun ccls/callee () (interactive) (lsp-ui-peek-find-custom "$ccls/call" '(:callee t)))
-(defun ccls/caller () (interactive) (lsp-ui-peek-find-custom "$ccls/call"))
-(defun ccls/vars (kind) (lsp-ui-peek-find-custom "$ccls/vars" `(:kind ,kind)))
-(defun ccls/base (levels) (lsp-ui-peek-find-custom "$ccls/inheritance" `(:levels ,levels)))
-(defun ccls/derived (levels) (lsp-ui-peek-find-custom "$ccls/inheritance" `(:levels ,levels :derived t)))
-(defun ccls/member (kind) (interactive) (lsp-ui-peek-find-custom "$ccls/member" `(:kind ,kind)))
-
-;; References w/ Role::Role
-(defun ccls/references-read () (interactive)
-  (lsp-ui-peek-find-custom "textDocument/references"
-    (plist-put (lsp--text-document-position-params) :role 8)))
-
-;; References w/ Role::Write
-(defun ccls/references-write ()
-  (interactive)
-  (lsp-ui-peek-find-custom "textDocument/references"
-   (plist-put (lsp--text-document-position-params) :role 16)))
-
-;; References w/ Role::Dynamic bit (macro expansions)
-(defun ccls/references-macro () (interactive)
-  (lsp-ui-peek-find-custom "textDocument/references"
-   (plist-put (lsp--text-document-position-params) :role 64)))
-
-;; References w/o Role::Call bit (e.g. where functions are taken addresses)
-(defun ccls/references-not-call () (interactive)
-  (lsp-ui-peek-find-custom "textDocument/references"
-   (plist-put (lsp--text-document-position-params) :excludeRole 32)))
-;lsp mapping
-(map!
- ;; :after lsp
- :leader
- :prefix "l"
- :desc "lsp-find-definition" "d" #'lsp-find-definition
- :desc "lsp-format"          "f" #'lsp-format-buffer
- :desc "lsp-find-references" "r" #'lsp-find-references
- :desc "lsp-ui-imenu"        "i" #'lsp-ui-imenu
- :desc "lsp-rename"          "n" #'lsp-rename
-
- ;;navigation
- :desc "next-func" "j" #'my/next-func
- :desc "prev-func" "k" #'my/prev-func
-
- :desc "find-related-file"   "o" #'ff-find-related-file
- :desc "find-related-file-other-window" "O" #'projectile-find-other-file-other-window)
+(load "~/.doom.d/ccls_lspCfg.el")
+;; (load "~/.doom.d/clangd_lspCfg.el")
 
 ;; Window Navigation (faster using hydras)
 ;; (defhydra hydra-move (:body-pre (evil-window-left 1))
@@ -466,6 +419,8 @@ currently clocked-in org-mode task."
 ;; Compilation mode
 ;;
 (map! :leader
+        :desc "Make Frame"  "m"  #'make-frame-on-monitor)
+(map! :leader
       (:prefix "c"
         :desc "ivy/compile"  "C"  #'compile
         :desc "my/ivy/compile"  "d"  #'my/ivy/compile
@@ -480,8 +435,8 @@ currently clocked-in org-mode task."
                 "docker exec -it bob /bin/bash -c \"cd /shared/kinetis && make -f Make231857 SW_REV=Y -j -B\""
                 "docker exec -it bob /bin/bash -c \"cd /shared/kinetis && make -f Make231857 SW_REV=X -j -B\""
                 "docker exec -it bob /bin/bash -c \"cd /shared/kinetis && make -f Make217750 SW_REV=Y -j -B\""
-                "docker exec -it bob /bin/bash -c \"cd /shared/kinetis && make -f Make231857 SW_REV=X -j -B\" | compiledb"
-                "docker exec -it bob /bin/bash -c \"cd /shared/kinetis && make -f Make231857 SW_REV=Y -j -B\" | compiledb"
+                "cd ~/kinetis && docker exec -it bob /bin/bash -c \"cd /shared/kinetis && make -f Make231857 SW_REV=X -j -B\" | compiledb"
+                "cd ~/kinetis && docker exec -it bob /bin/bash -c \"cd /shared/kinetis && make -f Make231857 SW_REV=Y -j -B\" | compiledb"
                 "docker exec -it bob /bin/bash -c \"cd /shared/kinetis && make -f MakeLibraryModule SW_REV=X -j -B\""
                 "docker exec -it bob /bin/bash -c \"cd /shared/kinetis && make -f MakeTowerK60F SW_REV=X -j -B\""
                 "docker exec -it bob /bin/bash -c \"cd /shared/kinetis && make -f Make75177 SW_REV=Y -j -B\" && scp /home/reggiemarr/kinetis/231857-01X.axf rmarr@pyrite:/home/bdi3000/rmarr/"
@@ -614,7 +569,23 @@ concat (concat "\nstatic void " (concat snip_str "(int type, void *argPtr, int r
 }
 ")
                         )
-                ))
+                )
+        (defun maybe_notify_snip (snip_str)
+          "This function conditionally returns a string"
+                (if (string= snip_str "0")
+                     " "
+                        (
+concat (concat "\nstatic void " (concat snip_str "(int type, void *argPtr, int recordNum)"))
+"\n{
+        if (type == NOTIFY_GET) {
+        }
+        else (type == NOTIFY_SET) {
+        }
+}
+")
+                        )
+                )
+)
 
 (use-package! org
   :init
